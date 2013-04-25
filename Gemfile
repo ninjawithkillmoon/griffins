35f800a38c96d715b60b21f21a1ba2980d8fac89
John Tonkin <john.william.tonkin@gmail.com>
2013-04-25 17:24:16 +0930

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'jquery-rails'
+
+group :development do
+  gem 'sqlite3'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NwkmGriffins::Application.config.secret_token = '5f92bfc2cfe49bfe778401526f796ad773d0dd7e1c57c934a83df596f64954fc2bf52cf47ce0f1c0fd313316cbfe13ec0005678f0cb4de91e01bfd4385a70b1a'
